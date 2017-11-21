f5866bf4f6b406fa4cf532b8492bda16aa89bccd
Tasuku Kubo <e175764@ie.u-ryukyu.ac.jp>
2017-11-21 09:40:40 +0900

    Third

--- src/jp/ac/uryukyu/ie/e175764/Main.java
+++ src/jp/ac/uryukyu/ie/e175764/Main.java
@@ -2,15 +2,7 @@ package jp.ac.uryukyu.ie.e175764;
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        try {
-            System.out.println(str.length());
-        }
-        catch (NullPointerException error) {
-            System.out.println("NullPointerException is occurring");
-            System.out.println(error.getMessage());
-        }
-        }
-
-
+        String str = "3．14";
+        double value=Double.parseDouble(str);
+     }
 }
